24127a0cea7b7904884fb0e1c605ac496cce27e5
Neakit <pavliukny@gmail.com>
2019-03-05 19:26:29 +0200

    changed image size and position

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,6 +1,8 @@
 <template>
     <v-container>
         <h1 class="text-xs-center">Vue + Laravel Project</h1>
-        <img src="https://warlord0blog.files.wordpress.com/2018/03/vue-laravel.png?w=712" alt="title" width="50%">
+        <v-card class="text-xs-center">
+            <img src="https://warlord0blog.files.wordpress.com/2018/03/vue-laravel.png?w=712" alt="title" width="50%">
+        </v-card>
     </v-container>
 </template>
